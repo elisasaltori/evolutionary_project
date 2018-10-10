5bc502ead6bdaad081eb5890ad6d5e786528ef39
elisasaltori <elisa.trujillo@usp.br>
2018-10-10 10:15:03 -0300

    added red ball script

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    Vector3 initialPosition; //save position for resetting level
+    public float speed = 2.5f;
+
+	// Use this for initialization
+	void Start () {
+        initialPosition = this.transform.position;
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MoveHorizontal();
+	}
+
+    void MoveHorizontal()
+    {
+        transform.Translate(Vector3.right * speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Border")
+            speed *= -1;
+    }
+
+
+
+
+}
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     public bool playerControlled = true;
-    public float speed = 0.1f;
+    public float speed = 3f;
     Rigidbody2D rb;
 
 
@@ -22,20 +22,13 @@ public class PlayerController : MonoBehaviour {
 
     void MoveInput()
     {
-        /*
-        if (Input.GetKey("up"))
-            rb.MovePosition(transform.position + Vector3.up*speed);
-        
-        if (Input.GetKey("down"))
-            rb.MovePosition(transform.position + Vector3.down * speed);
-     
-        if (Input.GetKey("left"))
-            rb.MovePosition(transform.position + Vector3.left * speed);
-   
-        if (Input.GetKey("right"))
-            rb.MovePosition(transform.position + Vector3.right * speed);*/
-
         var v3 = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0.0f);
-        rb.MovePosition(transform.position + speed * v3.normalized * speed);
+        rb.MovePosition(transform.position + speed * v3.normalized * speed *Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //if (collision.gameObject.tag == "Enemy")
+            //print("I die :(");
     }
 }
